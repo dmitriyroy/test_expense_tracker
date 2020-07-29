e331d9ce154dd2d3f0fe6e9be0217668008c8d43
dmitriyroy <dmitriy.roy@ita.biz.ua>
2020-07-29 21:06:53 +0300

    add models to project

--- src/main/java/dmroy/expensetracker/controller/HomeController.java
+++ src/main/java/dmroy/expensetracker/controller/HomeController.java
@@ -1,6 +1,5 @@
 package dmroy.expensetracker.controller;
 
-import dmroy.expensetracker.model.Expense;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
